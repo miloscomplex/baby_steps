cf37655a3e7167a53bbf8282f8b50e86fa189717
miloscomplex <hicallmesutton@gmail.com>
2020-09-25 16:28:12 -0500

    migrated care_givers table

--- db/migrate/20200925211638_care_givers.rb
+++ db/migrate/20200925211638_care_givers.rb
@@ -1,4 +1,9 @@
 class CareGivers < ActiveRecord::Migration
   def change
+    create_table :care_givers do |t|
+      t.string :name
+      t.string :user_name
+      t.string :password 
+    end
   end
 end
